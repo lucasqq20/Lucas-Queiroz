a948a3c9a3bf0c3611e7940e699583a4429db665
lucasqq20 <136238637+lucasqq20@users.noreply.github.com>
2023-06-11 12:06:42 -0300

    Delete docs directory

--- docs/search/files_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['f1_2ec_0',['f1.c',['../f1_8c.html',1,'']]]
-];
--- docs/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['documenta_26ccedil_3b_26atilde_3bo_20fun_26ccedil_3b_26otilde_3bes_20da_20biblioteca_20do_20higor_0',['Documenta&amp;ccedil;&amp;atilde;o Fun&amp;ccedil;&amp;otilde;es da Biblioteca do Higor',['../index.html',1,'']]]
-];
