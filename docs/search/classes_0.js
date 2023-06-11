112f4dfa7cad55838718a126408d5fad8e08fb51
lucasqq20 <136238637+lucasqq20@users.noreply.github.com>
2023-06-11 11:59:48 -0300

    Delete docs directory

--- docs/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['microsd_0',['MicroSD',['../struct_micro_s_d.html',1,'']]]
-];
--- docs/search/functions_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['microsd_5fabrirarquivo_0',['microSD_abrirArquivo',['../_biblioteca_8c.html#a5bfa401d00345951f80d14b1048c6804',1,'Biblioteca.c']]],
-  ['microsd_5fescrevernoarquivo_1',['microSD_escreverNoArquivo',['../_biblioteca_8c.html#ae06290d69585ce7c16903c3509323089',1,'Biblioteca.c']]],
-  ['microsd_5flerdoarquivo_2',['microSD_lerDoArquivo',['../_biblioteca_8c.html#a4ce532dfbe1838779f9fead57789aa4a',1,'Biblioteca.c']]],
-  ['microsd_5fmontar_3',['microSD_montar',['../f1_8c.html#a19ce3201416bd53b49b5d38efab51918',1,'f1.c']]]
-];
