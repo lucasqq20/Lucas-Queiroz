0d4f51d5796c21e9ceda41cef88b7b4ef8022e72
lucasqq20 <136238637+lucasqq20@users.noreply.github.com>
2023-06-11 10:29:38 -0300

    Delete microsd directory

--- microsd/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['documentaçao_20funcoes_20da_20biblioteca_20do_20higor_0',['Documentaçao Funcoes da Biblioteca do Higor',['../index.html',1,'']]]
-];
--- microsd/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pinoselecaochip_0',['pinoSelecaoChip',['../struct_micro_s_d.html#adf2ab6b2a4cf27fda8d5d1a67459a458',1,'MicroSD']]]
-];
