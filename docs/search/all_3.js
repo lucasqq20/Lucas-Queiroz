4d49caa9f8157931d9c4cad4caddd007d78abb88
lucasqq20 <136238637+lucasqq20@users.noreply.github.com>
2023-06-11 10:46:41 -0300

    Add files via upload

---
+++ docs/search/all_3.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['main_20page_2ec_0',['Main Page.c',['../_main_01_page_8c.html',1,'']]],
+  ['microsd_1',['MicroSD',['../struct_micro_s_d.html',1,'']]],
+  ['microsd_5fabrirarquivo_2',['microSD_abrirArquivo',['../_biblioteca_8c.html#a5bfa401d00345951f80d14b1048c6804',1,'Biblioteca.c']]],
+  ['microsd_5fescrevernoarquivo_3',['microSD_escreverNoArquivo',['../_biblioteca_8c.html#ae06290d69585ce7c16903c3509323089',1,'Biblioteca.c']]],
+  ['microsd_5fh_4',['MICROSD_H',['../_biblioteca_8c.html#aa2c1757fa47cac86551a728b37bf8f42',1,'Biblioteca.c']]],
+  ['microsd_5flerdoarquivo_5',['microSD_lerDoArquivo',['../_biblioteca_8c.html#a4ce532dfbe1838779f9fead57789aa4a',1,'Biblioteca.c']]],
+  ['microsd_5fmontar_6',['microSD_montar',['../f1_8c.html#a19ce3201416bd53b49b5d38efab51918',1,'f1.c']]]
+];
---
+++ docs/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['documentaçao_20funcoes_20da_20biblioteca_20do_20higor_0',['Documentaçao Funcoes da Biblioteca do Higor',['../index.html',1,'']]]
+];
